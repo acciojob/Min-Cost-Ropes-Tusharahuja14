07e06ad2a245a14361736a6d7e56e76ce676281e
Tushar Ahuja <93457512+Tusharahuja14@users.noreply.github.com>
2024-12-28 00:01:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,18 @@ function mincost(arr)
 { 
 //write your code here
 // return the min cost
-  
+	arr.sort((a,b)=>a-b);
+	let totalcost=0;
+	while(arr.length>1)
+		{
+			let small1=arr.shift();
+			let small2=arr.shift();
+			let count=small1+small2;
+			totalcost+=count;
+			arr.pus(count);
+			arr.sort((a,b)=>a-b);
+		}
+	return totalcost;
 }
 
 module.exports=mincost;
